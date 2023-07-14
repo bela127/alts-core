26c016817a61ecd58ae84681a713ed7e1b6ea548
bela127 <bhb127@outlook.de>
2023-07-14 16:39:02 +0200

    Move query specific stuff from experiment to process

--- alts/core/experiment.py
+++ alts/core/experiment.py
@@ -30,43 +30,25 @@ class Experiment():
         self.iteration = 0
 
 
-    def run(self) -> int:
-        self.time_dependent_loop(self.iteration)
+    def init_query(self):
         queries = self.initial_query_sampler.sample()
         self.oracles.process.add(queries)
+        return queries
+
+    def run(self) -> int:
+        self.time_source.step(self.iteration)
+        self.process.stream_update()
+
+        self.init_query()
 
         while True:
-            self.query_dependent_loop()
+            self.process.step()
+            self.experiment_modules.run()
             self.iteration += 1
 
             if not self.stopping_criteria.next: break
 
-            self.time_dependent_loop(self.iteration)
+            self.time_source.step(self.iteration)
+            self.process.stream_update()
             
         return self.exp_nr
-
-    def time_dependent_loop(self, iteration: int):
-        times = self.time_source.step(iteration)
-        times, vars = self.process.time_behavior.query(times)
-        self.data_pools.stream.add((times, vars))
-        return times, vars
-
-    def query_dependent_loop(self):
-        queries = None
-        results = None
-        delayed_queries = None
-        delayed_results = None
-
-        if not self.oracles.process.empty and self.process.ready:
-                queries = self.oracles.process.pop()
-                queries, results = self.process.query(queries)
-                self.data_pools.process.add((queries, results))
-
-        self.process.update()
-        
-        if self.process.has_new_data:
-            delayed_queries, delayed_results = self.process.delayed_results()
-            self.data_pools.result.add((delayed_queries, delayed_results))
-
-        self.experiment_modules.run()
-        return queries, results, delayed_queries, delayed_results
