003ca0601effefacab3fed38a5ede55cab83c956
bela127 <bhb127@outlook.de>
2023-07-14 12:28:47 +0200

    query_selector update to new subscriber names and interface

--- alts/core/query/query_selector.py
+++ alts/core/query/query_selector.py
@@ -3,24 +3,26 @@ from typing import TYPE_CHECKING
 
 from dataclasses import dataclass
 
-from alts.core.subscriber import ProcessSubscriber, ResultSubscriber, StreamSubscriber
+from alts.core.subscriber import ProcessDataSubscriber, ResultDataSubscriber, StreamDataSubscriber
 from alts.core.query.query_optimizer import QueryOptimizer
 from alts.core.query.query_decider import QueryDecider
 from alts.core.configuration import Required, init
+from alts.core.experiment_module import ExperimentModule
+
 
 if TYPE_CHECKING:
     from typing_extensions import Self #type: ignore
     from alts.core.configuration import Required
-    from alts.core.experiment_modules import ExperimentModules
+    from alts.core.subscribable import Subscribable
 
 @dataclass
-class QuerySelector(StreamSubscriber, ProcessSubscriber, ResultSubscriber):
+class QuerySelector(ExperimentModule, StreamDataSubscriber, ProcessDataSubscriber, ResultDataSubscriber):
     query_optimizer: QueryOptimizer = init()
     query_decider: QueryDecider = init()
 
     def __post_init__(self):
         super().__post_init__()
-        self.query_optimizer = self.query_optimizer( exp_modules = self.exp_modules)
+        self.query_optimizer = self.query_optimizer(exp_modules = self.exp_modules)
         self.query_decider = self.query_decider(exp_modules = self.exp_modules)
 
 
@@ -32,9 +34,9 @@ class QuerySelector(StreamSubscriber, ProcessSubscriber, ResultSubscriber):
     
 class ResultQuerySelector(QuerySelector):
     
-    def result_update(self):
-        super().result_update()
+    def result_update(self, subscription: Subscribable):
+        super().result_update(subscription)
         self.decide()
 
-    def update(self) -> None:
-        super().update()
+    def update(self, subscription: Subscribable) -> None:
+        super().update(subscription)
