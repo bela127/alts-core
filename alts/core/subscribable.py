a28968b6eab592e2b75bb590ef7e4595521e4927
bela127 <bhb127@outlook.de>
2023-07-14 12:24:25 +0200

    subscribable make it a Configurable; add better Error message

--- alts/core/subscribable.py
+++ alts/core/subscribable.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from alts.core.configuration import Configurable
 
 if TYPE_CHECKING:
     from typing import List, Tuple, Callable, Optional
-
     from alts.core.subscriber import Subscriber
 
-class Subscribable():
+class Subscribable(Configurable):
 
     def __init__(self):
-        super().__init__()
+        self.__post_init__()
+
+    def __post_init__(self):
+        super().__post_init__()
         self.__subscriber: List[Tuple[Subscriber,Callable[[],None]]] = []
         
     def subscribe(self, subscriber: Subscriber, callable: Optional[Callable[[],None]] = None):
@@ -21,4 +24,7 @@ class Subscribable():
     
     def update(self):
         for subscriber, callable in self.__subscriber:
-            callable()
+            try:
+                callable(self)
+            except TypeError as e:
+                raise TypeError(f"The subscriber '{subscriber.__class__}' should implement a callable {str(callable)} with an 'subscription' argument") from e
