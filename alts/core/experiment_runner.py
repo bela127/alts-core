d65305dc2e7aa8d40ea4748e583300600a224593
bela127 <bhb127@outlook.de>
2023-08-04 19:07:30 +0200

    experiment_runner.py parallel sub experiments as standart

--- alts/core/experiment_runner.py
+++ alts/core/experiment_runner.py
@@ -32,7 +32,29 @@ class ExperimentRunner():
             
             print("Running:", experiment.exp_name, exp_nr)
             experiment.run()
+
+    def run_repetition(self, exp_config: Tuple[Blueprint, int]):
+        blueprint, exp_nr = exp_config
+        evaluators: List[Evaluator] = []
+        for evaluator in blueprint.evaluators:
+            evaluators.append(evaluator())
         
+        experiment = Experiment(blueprint, exp_nr)
+        
+        for evaluator in evaluators:
+            evaluator.register(experiment)
+        
+        print("Running:", experiment.exp_name, exp_nr)
+        experiment.run()
+        
+    
+    def run_experiment_parallel(self, blueprint: Blueprint, nr_processes: int = int(os.cpu_count() / 2)):
+        exp_configs = []
+        for exp_nr in range(blueprint.repeat):
+            exp_configs.append((blueprint,exp_nr))
+        
+        with Pool(nr_processes) as p:
+            p.map(self.run_repetition, exp_configs)
 
     def run_experiments(self, blueprints: Union[Iterable[Blueprint], None] = None):
         if blueprints is None: blueprints = self.blueprints
@@ -40,11 +62,15 @@ class ExperimentRunner():
         for blueprint in blueprints:
             self.run_experiment(blueprint)
 
-    def run_experiments_parallel(self, blueprints: Union[Iterable[Blueprint], None] = None, nr_processes: int = int(os.cpu_count() / 2)):
+    def run_experiments_parallel(self, blueprints: Union[Iterable[Blueprint], None] = None, nr_processes: int = int(os.cpu_count() / 2), parallel_sub_exp=True):
         if blueprints is None: blueprints = self.blueprints
-
-        with Pool(nr_processes) as p:
-            p.map(self.run_experiment, blueprints)
+        if parallel_sub_exp:
+            for blueprint in blueprints:
+                self.run_experiment_parallel(blueprint)
+        else:
+            with Pool(nr_processes) as p:
+                p.map(self.run_experiment, blueprints)
+    
 
 
     def register_evaluators(self, experiment):
