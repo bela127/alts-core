90f3e7b06baf562881d82271a0ed71d9bf6fc937
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-07-20 17:48:45 +0200

    Fully documented query decider

--- alts/core/query/query_decider.py
+++ alts/core/query/query_decider.py
@@ -1,4 +1,7 @@
-#TODO NID
+#Fully documented as of 20.07.2024
+"""
+:doc:`Built-In Implementations </core/query/query_decider>`
+"""
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
@@ -14,7 +17,24 @@ if TYPE_CHECKING:
 
 @dataclass
 class QueryDecider(ExperimentModule, QueryConstrained):
+    """
+    | **Description**
+    |   This module decides which next query would bring the most valuable information to the table.
+    """
 
     @abstractmethod
     def decide(self, query_candidates: NDArray[Shape["query_nr, ... query_dims"], Number], scores: NDArray[Shape["query_nr, [query_score]"], Number]) -> Tuple[bool, NDArray[Shape["query_nr, ... query_dims"], Number]]: # type: ignore
+        """
+        | **Description**
+        |   Returns its favorite query/queries out of a list of candidates with associated scores from 0 to 1.
+        |   Is not implemented here.
+
+        :param query_candidates: A list of queries to choose from
+        :type query_candidates: Iterable over `NDArrays <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_ 
+        :param scores: A list of scores associated to the queries in ``query_candidates``
+        :type scores: Iterable over `NDArrays <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_ 
+        :return: Whether it wants to decide, Favorite query/queries
+        :rtype: ``boolean``, Iterable over `NDArrays <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_ 
+        :raises: NotImplementedError
+        """
         raise NotImplementedError()
