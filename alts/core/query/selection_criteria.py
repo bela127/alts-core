2088d97b1d3abcfaffb686290c7081ef3144af5f
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-07-20 17:48:56 +0200

    Fully documented selection_criteria

--- alts/core/query/selection_criteria.py
+++ alts/core/query/selection_criteria.py
@@ -1,4 +1,7 @@
-#TODO NID
+#Fully documented as of 20.07.2024
+"""
+:doc:`Built-In Implementations </module/query/selection_criteria>`
+"""
 from __future__ import annotations
 from abc import abstractmethod, abstractproperty
 from typing import TYPE_CHECKING
@@ -15,10 +18,35 @@ if TYPE_CHECKING:
 
 
 class SelectionCriteria(ExperimentModule, Queryable):
+    """
+    | **Description**
+    |   A ``SelectionCriteria`` is a way by which to score query candidates on a scale from 0 to 1.
+    """
 
     @abstractmethod
     def query(self, queries: NDArray[Shape["query_nr, ... query_shape"], Number]) -> Tuple[NDArray[Shape["query_nr, ... query_shape"], Number], NDArray[Shape["query_nr, [score]"], Number]]: # type: ignore
+        """
+        | **Description**
+        |   Gives each query from the list a score based on the implementation.
+        |   Is not implemented here.
+
+        :param queries: A list of queries to evaluate
+        :type queries: Iterable over `NDArrays <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_ 
+        :return: queries, associated scores
+        :rtype: Iterable over `NDArrays <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_, Iterable over `NDArrays <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_ 
+        :raises: NotImplementedError
+        """
         raise NotImplementedError
     
     def result_constrain(self) -> ResultConstrain:
+        """
+        | **Description**
+        |   Returns the result constraints for scores.
+
+        | **Current Constraints**
+        |   *Value Range:* [0, 1] for all values
+
+        :return: Constraints around results
+        :rtype: ResultConstrain
+        """
         return ResultConstrain((self.query_constrain().shape[0], 1))
