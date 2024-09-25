8b7c4baf86f5e7f1670c0e4043a48f4ffce6e061
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-09-25 15:50:08 +0200

    Queryable fully documented

--- alts/core/query/queryable.py
+++ alts/core/query/queryable.py
@@ -1,4 +1,4 @@
-#TODO NID
+#Fully documented as of 25.09.2024
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Protocol
@@ -16,7 +16,19 @@ if TYPE_CHECKING:
     from nptyping import NDArray, Shape, Number
 
 class Queryable(Constrained):
-
+    """
+    | **Description**
+    |   A Queryable module can be queried for results.
+    """
     @abstractmethod
     def query(self, queries: NDArray[Shape["query_nr, ... query_shape"], Number]) -> Tuple[NDArray[Shape["query_nr, ... query_shape"], Number], NDArray[Shape["query_nr, ... result_shape"], Number]]: # type: ignore
+        """
+        | **Description**
+        |   For a list of queries it returns a list of queries with associated results.
+
+        :param queries: A list of queries
+        :type queries: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        :return: A tuple of queries and their results
+        :rtype: Tuple[`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_,`NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_]
+        """
         raise NotImplementedError()
