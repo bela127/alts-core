6df6439cef5431f2a104b7f0fb9df4b2b75ffe20
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-09-25 15:50:25 +0200

    Query Queue mostly documented

--- alts/core/oracle/query_queue.py
+++ alts/core/oracle/query_queue.py
@@ -17,6 +17,10 @@ if TYPE_CHECKING:
 
 @dataclass
 class QueryQueue(DelayedPublisher, QueryConstrained):
+    """
+    | **Description**
+    |   A buffer for queries in shape of a query. 
+    """
     queries: NDArray[Shape["query_nr, ... query_shape"], Number] = post_init() # type: ignore
     _query_constrain: QueryConstrainedGetter = post_init()
 
@@ -25,15 +29,35 @@ class QueryQueue(DelayedPublisher, QueryConstrained):
 
 
     def post_init(self):
+        """
+        | **Description**
+        |   Initialites an empty query queue.
+        """
         super().post_init()
         self.queries = np.empty((0, *self.query_constrain().shape))
 
     def add(self, queries: NDArray[Shape["query_nr, ... query_shape"], Number]): # type: ignore
+        """
+        | **Description**
+        |   Adds the queries to the queue.
+
+        :param queries: The queries to be added to the queue
+        :type queries: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        """
         self.queries = np.concatenate((self.queries, queries))
         self._latest_add = queries[-1:]
         self.request_update()
     
     def pop(self, query_nr = 1) -> NDArray[Shape["query_nr, ... query_shape"], Number]: # type: ignore
+        """
+        | **Description**
+        |   Pops all queries up to the given query_nr (default is 1).
+
+        :param query_nr: How many queries to pop.
+        :type query_nr: int
+        :return: Array of popped queries
+        :rtype: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        """
         poped = self.queries[:query_nr,...]
         self.queries = self.queries[query_nr:,...]
         self._latest_pop = poped
@@ -41,29 +65,78 @@ class QueryQueue(DelayedPublisher, QueryConstrained):
 
     @property
     def last(self)-> NDArray[Shape["1, ... query_shape"], Number]: # type: ignore
+        """
+        | **Description**
+        |   Returns the last query of the queue.
+
+        :return: Last queue query
+        :rtype: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        """
         return self.queries[-1:,...]
     
     @property
     def first(self)-> NDArray[Shape["1, ... query_shape"], Number]: # type: ignore
+        """
+        | **Description**
+        |   Returns the first query of the queue.
+
+        :return: First queue query
+        :rtype: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        """
         return self.queries[:1,...]
 
     @property
     def latest_add(self)-> NDArray[Shape["1, ... query_shape"], Number]: # type: ignore
+        """
+        | **Description**
+        |   Returns the last added query.
+
+        :return: Last added query
+        :rtype: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        """
         return self._latest_add
     
     @property
     def latest_pop(self)-> NDArray[Shape["1, ... query_shape"], Number]: # type: ignore
+        """
+        | **Description**
+        |   Returns the last popped query.
+
+        :return: Last popped query
+        :rtype: `NDArray <https://numpy.org/doc/stable/reference/arrays.ndarray.html>`_
+        """
         return self._latest_pop
 
     @property
     def empty(self):
+        """
+        | **Description**
+        |   Returns ``True`` only if the queue is empty.
+
+        :return: Is the queue empty?
+        :rtype: bool
+        """
         return self.queries.shape[0] == 0
     
     @property
     def count(self):
+        """
+        | **Description**
+        |   Returns the size of the queue.
+
+        :return: Size of queue
+        :rtype: int
+        """
         return self.queries.shape[0]
 
     def query_constrain(self) -> QueryConstrain:
+        """
+        | **Description**
+        |   Returns its query constraints.
+
+        :return: Own query constraints
+        :rtype: :doc:`QueryConstrain </core/data/constrains>`
+        """
         return self._query_constrain()
        
 
