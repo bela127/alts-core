522f5409d5723d99ef90c1fa2ac2ce2c5f8276b0
Benjamin Gors <benjamin.gorskis@gmail.com>
2024-09-27 18:03:43 +0200

    Data Pools Full Dokumentation

--- alts/core/data/data_pools.py
+++ alts/core/data/data_pools.py
@@ -1,4 +1,4 @@
-#TODO D
+#Fully documented as of 27.09.2024
 from __future__ import annotations
 from typing import TYPE_CHECKING, Any
 
@@ -10,7 +10,7 @@ from alts.core.configuration import Configurable, is_set, init, post_init, pre_i
 class DataPools(Configurable):
     """
     | **Description**
-    |   A ``DataPools`` stores all queries and results that have been supplied to or requested by the trained model.
+    |   A ``DataPools`` stores all queries and results that have been supplied to it.
     """
     def trigger_subscriber(self):
         """
@@ -49,27 +49,49 @@ class StreamDataPools(DataPools):
 
 @dataclass
 class ResultDataPools(DataPools):
+    """
+    | **Description**
+    |   A ``DataPools`` specifically for results.
+    """
     result: QueriedDataPool = init()
 
     def trigger_subscriber(self):
+        """
+        | **Description**
+        |   Updates its own available data.
+        """
         super().trigger_subscriber()
         self.result.update()
 
 
 @dataclass
 class ProcessDataPools(DataPools):
+    """
+    | **Description**
+    |   A ``DataPools`` specifically for results.
+    """
     process: QueriedDataPool = init()
 
     def trigger_subscriber(self):
+        """
+        | **Description**
+        |   Updates its own available data.
+        """
         super().trigger_subscriber()
         self.process.update()
 
 @dataclass
 class PRDataPools(ResultDataPools, ProcessDataPools):
-    #ProcessResult
+    """
+    | **Description**
+    |   A ``DataPools`` specifically for process results.
+    """
     pass
 
 @dataclass
 class SPRDataPools(StreamDataPools, PRDataPools):
-    #StreamProcessResult
+    """
+    | **Description**
+    |   A ``DataPools`` specifically for stream process results.
+    """
     pass
